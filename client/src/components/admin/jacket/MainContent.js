6ee8cb36877e27093f365bf9837ffee2a3356973
khanh <khanh451754@gmail.com>
2020-12-17 09:45:32 +0700

    fix add product

--- client/src/components/admin/jacket/MainContent.js
+++ client/src/components/admin/jacket/MainContent.js
@@ -25,11 +25,12 @@ export default class MainContent extends React.Component{
    
     }
 
-    addProduct=(name,image,price)=>{
+    addProduct=(name,image,price,size)=>{
         axios.post('http://localhost:8080/jacket',{
             name,
             image,
-            price
+            price,
+            size
         },{
             headers:{
                 token: window.localStorage.getItem('admin_token')
