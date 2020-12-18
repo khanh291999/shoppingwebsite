a42b79eb2a9770cd26141d315c8705c33d26b01d
khanh <khanh451754@gmail.com>
2020-12-18 12:16:24 +0700

    update sidebar

--- client/src/components/admin/femalejean/SidebarFemaleJean.js
+++ client/src/components/admin/femalejean/SidebarFemaleJean.js
@@ -23,12 +23,20 @@ export default function SidebarFemaleJean(){
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const [openfemale, setOpenFemale] = React.useState(true);
+  const [openDisable, setOpenDisable] = React.useState(true);
+  const [openfemaleDisable, setOpenFemaleDisable] = React.useState(true);
 
   const handleClick = () => {
     setOpen(!open);
   };
   const handleClickFemale = () => {
     setOpenFemale(!openfemale);
+  };
+  const handleClickDisable = () => {
+    setOpenDisable(!openDisable);
+  };
+  const handleClickFemaleDisable = () => {
+    setOpenFemaleDisable(!openfemaleDisable);
   };
     return  <div className="sidebar">
             <List
@@ -93,6 +101,77 @@ export default function SidebarFemaleJean(){
                     <ListItemText primary="Jean" />
                   </ListItem>
                   <ListItem button component={Link} to="/adminfemalet-shirt"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="T-shirt" />
+                  </ListItem>
+                </List>
+              </Collapse>
+            </List>
+
+            <List
+              component="nav"
+              ria-labelledby="nested-list-subheader"
+              subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+              Disable Product
+              </ListSubheader>
+              }
+                className={classes.root}
+              >
+              <ListItem button onClick={handleClickDisable}>
+                <ListItemIcon>
+                  <CheckIcon />
+                </ListItemIcon>
+                <ListItemText primary="Male" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItem button component={Link} to="/disablejacket"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Jacket" />
+                  </ListItem>
+                  <ListItem button component={Link} to="/disablejean"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Jean" />
+                  </ListItem>
+                  <ListItem button component={Link} to="/disablet-shirt"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="T-shirt" />
+                  </ListItem>
+                </List>
+              </Collapse>
+              
+              <ListItem button onClick={handleClickFemaleDisable}>
+                <ListItemIcon>
+                  <CheckIcon />
+                </ListItemIcon>
+                <ListItemText primary="Female" />
+                {openfemale ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={openfemale} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                <ListItem button component={Link} to="/disablefemalejacket"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Jacket" />
+                  </ListItem>
+                  <ListItem button component={Link} to="/disablefemalejean"  className={classes.nested}>
+                    <ListItemIcon>
+                      <AddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Jean" />
+                  </ListItem>
+                  <ListItem button component={Link} to="/disablefemalet-shirt"  className={classes.nested}>
                     <ListItemIcon>
                       <AddIcon />
                     </ListItemIcon>
