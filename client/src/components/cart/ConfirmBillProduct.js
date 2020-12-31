6c877fb834c5802e6363253d2e2ca81771d3ce9b
DC99 <huuquyen1412@gmail.com>
2020-12-31 14:17:29 +0700

    FE - Check out page

--- client/src/components/cart/ConfirmBillProduct.js
+++ client/src/components/cart/ConfirmBillProduct.js
@@ -4,16 +4,18 @@ import React from 'react'
 export default function ConfirmBillProduct(props){
     const {img,name,price,size,quantity} = props.cart;
     return (
-        <Grid container style={{margin: "20px 0", borderBottom:"1px solid black", padding: "10px"}}>
-            <Grid item md={4}>
-                <img style={{maxWidth: "30%"}} src={img} alt="cart img"></img>
-            </Grid> 
-            <Grid item md={8} >
-                <Typography variant="h5">{name} x {quantity}</Typography>
-                <Typography>Price: {price*quantity}$</Typography>
-                <Typography> Quantity: {quantity} </Typography>
-                <Typography>Size: {size}</Typography>
-            </Grid>
-        </Grid>
+        <div class="product">
+            <div>
+                <div class="img-container">
+                    <img src={img} alt="cart img"/>
+                    <div style={{display:"flex", flexDirection:"column", justifyContent: "center"}}>
+                        <div class="product-label">{name}</div>
+                        <div class="product-size">{"Size: " + size}</div>
+                    </div>
+                    <div class="absolute">{quantity}</div>
+                </div>
+            </div>
+            <div style={{alignSelf:"center"}}>{"$" + price*quantity}</div>
+        </div>
     )
 }
