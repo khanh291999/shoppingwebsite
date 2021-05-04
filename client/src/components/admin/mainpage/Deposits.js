cc4ae31e660087a74b2d0ce8099345483fd70b55
Khanh Do Quoc <khanh451754@gmail.com>
2021-05-04 10:06:35 +0700

    fix admin deposit

--- client/src/components/admin/mainpage/Deposits.js
+++ client/src/components/admin/mainpage/Deposits.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect }  from "react";
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
+import axios from "axios"
 
 function preventDefault(event) {
   event.preventDefault();
@@ -15,15 +16,30 @@ const useStyles = makeStyles({
 });
 
 export default function Deposits() {
+    var today = new Date(),
+      date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
   const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/cart").then(res=>{
+      setData(res.data)
+  })
+  }, []);
+  const allTotal = data.reduce((a,v) =>  a = a + v.allTotal , 0 )
+  
   return (
     <React.Fragment>
       <Title>Recent Deposits</Title>
       <Typography component="p" variant="h4">
-        $3,024.00
+        $ {allTotal}
       </Typography>
       <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
+        on {date}
       </Typography>
       <div>
         <Link color="primary" href="#" onClick={preventDefault}>
