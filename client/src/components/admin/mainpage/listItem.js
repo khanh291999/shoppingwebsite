0469c57d4f5d2c24730fd949f29cffa0f63f5a8b
khanh <khanh451754@gmail.com>
2021-06-24 17:36:19 +0700

    fix staff vs admin

--- client/src/components/admin/mainpage/listItem.js
+++ client/src/components/admin/mainpage/listItem.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LayersIcon from '@material-ui/icons/Layers';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import Menu, { SubMenu, Item as MenuItem } from 'rc-menu';
-import CustomMenu from './CustomMenu'
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import PersonIcon from '@material-ui/icons/Person';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import FaceIcon from '@material-ui/icons/Face';
-import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
-
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import PeopleIcon from "@material-ui/icons/People";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import LayersIcon from "@material-ui/icons/Layers";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import Menu, { SubMenu, Item as MenuItem } from "rc-menu";
+import CustomMenu from "./CustomMenu";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import PersonIcon from "@material-ui/icons/Person";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import FaceIcon from "@material-ui/icons/Face";
+import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
+import AdminContext from "../../../context/adminContext";
 // export const secondaryListItems = (
 //   <div>
 //     <ListSubheader inset>Products</ListSubheader>
@@ -25,95 +25,113 @@ import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 //   </div>
 // );
 
-export class MenuSideBar extends React.Component{
-  handleActiveDashboard = () =>{
-    const {changeActive} = this.props
-    console.log("abv",changeActive);
-    changeActive("dashboard")
-  }
+export class MenuSideBar extends React.Component {
+  static contextType = AdminContext;
+  handleActiveDashboard = () => {
+    const { changeActive } = this.props;
+    console.log("abv", changeActive);
+    changeActive("dashboard");
+  };
 
-  handleLogOut = () =>{
-    const {logout} = this.props
-    logout()
-  }
+  handleLogOut = () => {
+    const { logout } = this.props;
+    logout();
+  };
 
-  handleUserManagement = () =>{
-    const {changeActive} = this.props
-    changeActive("user")
-  }
-  
-  handleStaffManagement = () =>{
-    const {changeActive} = this.props
-    changeActive("staff")
-  }
+  handleUserManagement = () => {
+    const { changeActive } = this.props;
+    changeActive("user");
+  };
 
-  handleOrderStatus = () =>{
-    const {changeActive} = this.props
-    changeActive("order")
-  }
-  
-  handleProfile = () =>{
-    const {changeActive} = this.props
-    changeActive("profile")
-  }
+  handleStaffManagement = () => {
+    const { changeActive } = this.props;
+    changeActive("staff");
+  };
+
+  handleOrderStatus = () => {
+    const { changeActive } = this.props;
+    changeActive("order");
+  };
+
+  handleProfile = () => {
+    const { changeActive } = this.props;
+    changeActive("profile");
+  };
 
   render() {
-    return(
+    const admin = this.context.adminData.admin;
+    console.log("admin", admin);
+
+    return (
       <>
         <Divider />
         <List>
           <div>
             <ListItem button onClick={this.handleActiveDashboard}>
-              <ListItemIcon style={{opacity:"1"}}>
+              <ListItemIcon style={{ opacity: "1" }}>
                 <DashboardIcon />
               </ListItemIcon>
               <ListItemText primary="Dashboard" />
             </ListItem>
             <ListItem button onClick={this.handleOrderStatus}>
-              <ListItemIcon  style={{opacity:"1"}}>
+              <ListItemIcon style={{ opacity: "1" }}>
                 <ShoppingCartIcon />
               </ListItemIcon>
               <ListItemText primary="Orders" />
             </ListItem>
-          </div>  
+          </div>
         </List>
         <Divider />
         <List>
           <ListSubheader inset>Products</ListSubheader>
-          <CustomMenu changeActive={this.props.changeActive}/>
+          <CustomMenu changeActive={this.props.changeActive} />
         </List>
         <Divider />
-        <List>
-          <div>
-            <ListSubheader inset>User Management</ListSubheader>
-            <ListItem button onClick={this.handleUserManagement}>
-              <ListItemIcon style={{opacity:"1"}}>
-                <FaceIcon />
-              </ListItemIcon>
-              <ListItemText primary="User" />
-            </ListItem>
-            <ListItem button onClick={this.handleStaffManagement}>
-              <ListItemIcon style={{opacity:"1"}}>
-                <AssignmentIndIcon />
-              </ListItemIcon>
-              <ListItemText primary="Staff" />
-            </ListItem>
-          </div>  
-        </List>
+        {admin.type == 1 ? (
+          <List>
+            <div>
+              <ListSubheader inset>User Management</ListSubheader>
+              <ListItem button onClick={this.handleUserManagement}>
+                <ListItemIcon style={{ opacity: "1" }}>
+                  <FaceIcon />
+                </ListItemIcon>
+                <ListItemText primary="User" />
+              </ListItem>
+              <ListItem button onClick={this.handleStaffManagement}>
+                <ListItemIcon style={{ opacity: "1" }}>
+                  <AssignmentIndIcon />
+                </ListItemIcon>
+                <ListItemText primary="Staff" />
+              </ListItem>
+            </div>
+          </List>
+        ) : (
+          <List>
+            <div>
+              <ListSubheader inset>User Management</ListSubheader>
+              <ListItem button onClick={this.handleUserManagement}>
+                <ListItemIcon style={{ opacity: "1" }}>
+                  <FaceIcon />
+                </ListItemIcon>
+                <ListItemText primary="User" />
+              </ListItem>
+            </div>
+          </List>
+        )}
         <Divider />
         <List>
-            <ListItem button onClick={this.handleProfile}>
-              <ListItemIcon style={{opacity:"1"}}>
-                <PersonIcon />
-              </ListItemIcon>
-              <ListItemText primary="Profile" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon style={{opacity:"1"}}>
-                <ExitToAppIcon />
-              </ListItemIcon>
-              <ListItemText primary="Log out" onClick={this.handleLogOut}/>
-            </ListItem>
+          <ListItem button onClick={this.handleProfile}>
+            <ListItemIcon style={{ opacity: "1" }}>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon style={{ opacity: "1" }}>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText primary="Log out" onClick={this.handleLogOut} />
+          </ListItem>
         </List>
       </>
     );
--- client/src/components/admin/staffcontrol/MainContentStaffControl.js
+++ client/src/components/admin/staffcontrol/MainContentStaffControl.js
@@ -1,194 +1,216 @@
-import React from 'react'
-import ContentHeaderStaffControl from './ContentHeaderStaffControl'
-import '../../../assets/adminstaffcontrol.css'
-import {Empty} from '../Empty'
-import ModalStaffControl from './ModalStaffControl'
-import ProductRowStaffControl from './ProductRowStaffControl'
-import axios from 'axios';
-import Swal from 'sweetalert2'
-import ErrorNotice from '../../misc/ErrorNotice'
-export default class MainContentStaffControl extends React.Component{
-    state={
-        open:false,
-        admins : [
-            
-        ],
-        isEditting:undefined, //index
-        error:undefined
+import React from "react";
+import ContentHeaderStaffControl from "./ContentHeaderStaffControl";
+import "../../../assets/adminstaffcontrol.css";
+import { Empty } from "../Empty";
+import ModalStaffControl from "./ModalStaffControl";
+import ProductRowStaffControl from "./ProductRowStaffControl";
+import axios from "axios";
+import Swal from "sweetalert2";
+import ErrorNotice from "../../misc/ErrorNotice";
+export default class MainContentStaffControl extends React.Component {
+  state = {
+    open: false,
+    admins: [],
+    isEditting: undefined, //index
+    error: undefined,
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.intervalID);
+  }
+
+  getData = () => {
+    axios.get("http://localhost:8080/admin").then((res) => {
+      console.log(res);
+      this.setState({
+        admins: res.data,
+      });
+    });
+    this.intervalID = setTimeout(this.getData.bind(this), 5000);
+  };
+
+  addAdmin = async (email, password, passwordCheck, displayName, type) => {
+    try {
+      await axios.post("http://localhost:8080/admins/addadmin", {
+        email,
+        password,
+        passwordCheck,
+        displayName,
+        type,
+      });
+      Swal.fire({
+        title: "Create Successfully",
+        timer: 1000,
+        icon: "success",
+      });
+      console.log("ResponseRegister");
+    } catch (err) {
+      err.response.data.msg && this.setState({ error: err.response.data.msg });
     }
+  };
 
-    componentDidMount(){
-        this.getData();
+  updateAdmin = async (
+    id,
+    email,
+    password,
+    passwordCheck,
+    displayName,
+    type
+  ) => {
+    try {
+      await axios.patch(`http://localhost:8080/admins/updateadmin/${id}`, {
+        email,
+        password,
+        passwordCheck,
+        displayName,
+        type,
+      });
+      Swal.fire({
+        title: "Update Successfully",
+        timer: 1000,
+        icon: "success",
+      });
+      console.log("ResponseRegister");
+    } catch (err) {
+      Swal.fire({
+        title: "Update Unsuccessfully",
+        text: err.message,
+        timer: 1000,
+        icon: "error",
+      });
+      err.response.data.msg && this.setState({ error: err.response.data.msg });
     }
+  };
 
-    componentWillUnmount() {
-        clearTimeout(this.intervalID);
-      }
-
-    getData = () =>{
-        axios.get("http://localhost:8080/admin").then(res=>{
-            console.log(res);
-            this.setState({
-                admins: res.data
-            })
-        })
-        this.intervalID = setTimeout(this.getData.bind(this), 5000);
-    }
-
-    addAdmin= async(email, password, passwordCheck, displayName, type )=>{
-        try {
-          await axios.post("http://localhost:8080/admins/addadmin", {
-            email,
-            password,
-            passwordCheck,
-            displayName,
-            type,
-          });
-          Swal.fire({
-                    title:"Create Successfully",
-                    timer:1000,
-                    icon:'success'
-                })
-          console.log("ResponseRegister");
-        } catch (err) {
-          err.response.data.msg && this.setState({error:err.response.data.msg});
-        }
-    }
-
-    updateAdmin = async (id,email,password,passwordCheck,displayName,type) => {
-        try {
-            await axios.patch(`http://localhost:8080/admins/updateadmin/${id}`, {
-              email,
-              password,
-              passwordCheck,
-              displayName,
-              type,
-            });
-            Swal.fire({
-                      title:"Update Successfully",
-                      timer:1000,
-                      icon:'success'
-                  })
-            console.log("ResponseRegister");
-          } catch (err) {
-            Swal.fire({
-                title:"Update Unsuccessfully",
-                text:err.message,
-                timer:1000,
-                icon:'error'
-            })
-            err.response.data.msg && this.setState({error:err.response.data.msg});
-          }
-    }
-
-    deleteAdmin = (id) => {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'You will not be able to recover this product !',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Yes, Deleted it!',
-            cancelButtonText: 'No, keep it'
-          }).then((result) => {
-            if (result.value) {
-                axios.delete(`http://localhost:8080/admin/${id}`,{
-                    id
-                   },{
-                       headers:{
-                           token: window.localStorage.getItem('admin_token')
-                       }
-                   })
-              Swal.fire(
-                'Deleted!',
-                'Your product has been deleted.',
-                'success'
-              )
-            } else if (result.dismiss === Swal.DismissReason.cancel) {
-              Swal.fire(
-                'Cancelled',
-                'Your prodct is safe :)',
-                'error'
-              )
-            }
-          })
-        .then(res=>{
-            console.log(res)
-        }).catch(err=>{
-            console.log(err);
-            Swal.fire({
-                title:"Delete Unsuccessfully",
-                text:err.message,
-                timer:1000,
-                icon:'error'
-            })
-        })
-    }
-
-    updateIsEditting = (id) => {
-        const admin_index = this.state.admins.findIndex((admin)=>{
-            return admin.id === id
-        })
-        this.setState({
-            isEditting:admin_index
-        })
-        this.toggleModal();
-    }
-
-    toggleModal=()=>{
-        this.setState({
-            open:!this.state.open
-        })
-    }
-
-    clearIsEditing = () => {
-        this.setState({
-            isEditting:undefined
-        })
-    }
-
-    handleError=()=>{
-        this.setState({
-            error:undefined
-        })
-    }
-
-    render(){
-        return  <>
-        <main className='content'>
-                <ContentHeaderStaffControl toggleModal={this.toggleModal} addAdmin={this.addAdmin}/>    
-                {this.state.error && (
-                        <ErrorNotice message={this.state.error} clearError={() => this.handleError(undefined)} />
-                     )}
-                <div className="content-table">
-                    <div className="table-headers">
-                        <div className="table-header">
-                            Display Name
-                        </div>
-                        <div className="table-header">
-                            Password
-                        </div>
-                        <div className="table-header">
-                            Email
-                        </div>
-                        <div className="table-header">
-                            Type
-                        </div>
-                        <div className="table-header" style={{justifySelf:'center'}}>
-                            Action
-                        </div>
-                    </div>
-                    {
-                        this.state.admins.length>0?
-                        this.state.admins.map((admin)=>{
-                            return <ProductRowStaffControl updateIsEditting={this.updateIsEditting}  deleteAdmin={this.deleteAdmin}  key={`admin_id_${admin.id}`} admin={admin}/>
-                        })
-                        :<Empty/>
-                    }
-                </div>
-            </main>
+  deleteAdmin = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will not be able to recover this product !",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, Deleted it!",
+      cancelButtonText: "No, keep it",
+    })
+      .then((result) => {
+        if (result.value) {
+          axios.delete(
+            `http://localhost:8080/admin/${id}`,
             {
-                this.state.open?<ModalStaffControl updateAdmin={this.updateAdmin} clearIsEditing={this.clearIsEditing} editingAdmin={this.state.admins[this.state.isEditting]} addAdmin={this.addAdmin} toggleModal={this.toggleModal} error={this.state.error}/>:''
+              id,
+            },
+            {
+              headers: {
+                token: window.localStorage.getItem("admin_token"),
+              },
             }
-        </>
-    }
+          );
+          Swal.fire("Deleted!", "Your product has been deleted.", "success");
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire("Cancelled", "Your prodct is safe :)", "error");
+        }
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        Swal.fire({
+          title: "Delete Unsuccessfully",
+          text: err.message,
+          timer: 1000,
+          icon: "error",
+        });
+      });
+  };
+
+  updateIsEditting = (id) => {
+    const admin_index = this.state.admins.findIndex((admin) => {
+      return admin.id === id;
+    });
+    this.setState({
+      isEditting: admin_index,
+    });
+    this.toggleModal();
+  };
+
+  toggleModal = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+
+  clearIsEditing = () => {
+    this.setState({
+      isEditting: undefined,
+    });
+  };
+
+  handleError = () => {
+    this.setState({
+      error: undefined,
+    });
+  };
+
+  render() {
+    let adminss = this.state.admins.filter(function (admin) {
+      return admin.type == 0;
+    });
+    return (
+      <>
+        <main className="content">
+          <ContentHeaderStaffControl
+            toggleModal={this.toggleModal}
+            addAdmin={this.addAdmin}
+          />
+          {this.state.error && (
+            <ErrorNotice
+              message={this.state.error}
+              clearError={() => this.handleError(undefined)}
+            />
+          )}
+          <div className="content-table">
+            <div className="table-headers">
+              <div className="table-header">Display Name</div>
+              <div className="table-header">Password</div>
+              <div className="table-header">Email</div>
+              <div className="table-header">Type</div>
+              <div className="table-header" style={{ justifySelf: "center" }}>
+                Action
+              </div>
+            </div>
+            {adminss.length > 0 ? (
+              adminss.map((admin) => {
+                return (
+                  <ProductRowStaffControl
+                    updateIsEditting={this.updateIsEditting}
+                    deleteAdmin={this.deleteAdmin}
+                    key={`admin_id_${admin.id}`}
+                    admin={admin}
+                  />
+                );
+              })
+            ) : (
+              <Empty />
+            )}
+          </div>
+        </main>
+        {this.state.open ? (
+          <ModalStaffControl
+            updateAdmin={this.updateAdmin}
+            clearIsEditing={this.clearIsEditing}
+            editingAdmin={this.state.admins[this.state.isEditting]}
+            addAdmin={this.addAdmin}
+            toggleModal={this.toggleModal}
+            error={this.state.error}
+          />
+        ) : (
+          ""
+        )}
+      </>
+    );
+  }
 }
